ab446ab0075df7b92ab5c97f339a17460a331491
Eragon4715 <kirya3232@yandex.ru>
2025-04-19 19:38:08 +0300

    upd .gitignore

--- main.py
+++ main.py
@@ -14,10 +14,9 @@ from src.db.models import (
     DiseaseSymptomLink,
     HealthMetricCreate, HealthMetricResponse, PredictionRequest, PredictionResponse, disease_symptoms
 )
-
 from src.utils.users import (
     hash_password, verify_password, create_access_token, get_user_by_nickname,
-    get_users, get_current_user, NoteCreate, NoteResponse, create_note, get_notes_by_user,
+    get_users, get_current_user, create_note, get_notes_by_user,NoteCreate, NoteResponse,
     add_disease_to_user, get_user_diseases, create_symptom, link_disease_symptom
 )
 
@@ -240,9 +239,9 @@ async def add_disease(
 async def get_diseases(db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
     return await get_user_diseases(db, current_user.id)
 
-# Создание болезни
+# Создание болезни (без токена)
 @app.post("/disease", response_model=DiseaseResponse, tags=['Болезни'])
-async def create_disease(disease_data: DiseaseCreate, db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
+async def create_disease(disease_data: DiseaseCreate, db: AsyncSession = Depends(get_db)):
     result = await db.execute(select(Disease).filter(Disease.name == disease_data.name))
     existing_disease = result.scalars().first()
     if existing_disease:
